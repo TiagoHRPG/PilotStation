dd25fbb64b9ab65d99657e470d993794aff449e0
Tiago <thrpg@cin.ufpe.br>
2025-05-07 20:18:16 -0300

    create parameter method and use api url dinamically

--- pilot-frontend/src/services/client.ts
+++ pilot-frontend/src/services/client.ts
@@ -1,6 +1,17 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+
+// Função para determinar o IP base da API
+const getApiBaseUrl = (): string => {
+  // 2. Segunda prioridade: Detecção automática
+  const hostname = window.location.hostname;
+  const port = '8000'; // Porta da API backend
+  
+  return `http://${hostname}:${port}`;
+};
+
+
+export const API_BASE_URL = getApiBaseUrl();
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
